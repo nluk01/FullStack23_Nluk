ec2aa6e479bb0c6d2a189d07d53f03215a1e6953
Nun <nun.lukkarinen01@gmail.com>
2023-12-25 14:11:59 +0200

    Valmis

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,7 +1,61 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import React from 'react';
 
-export default App
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  );
+};
+
+const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+  return (
+    <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
+  );
+};
+
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  const totalExercises = exercises1 + exercises2 + exercises3;
+  return (
+    <p>Number of exercises {totalExercises}</p>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;

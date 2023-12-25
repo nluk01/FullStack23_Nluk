ad85dfed7e46cc9c83d631aaa3dc1a542fdc15f1
Nun <nun.lukkarinen01@gmail.com>
2023-12-25 14:25:18 +0200

    muokattu

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,20 +1,59 @@
-const Heaader = ({kurssi}) => {
-  <h1>{kurssi}</h1>
+const Header = ({kurssi}) => {
+  return (
+    <h1>{kurssi}</h1>
+  )
 }
 
-
-const App = () => {
-  const kurssi = 'Half Stack application development'
-
-
+const Content = ({part1, harjoitus1, part2, harjoitus2, part3, harjoitus3}) => {
   return (
     <div>
-      <Header kurssi={kurssi} />
-
+      <p>
+        {part1} {harjoitus1}
+      </p>
+      <p>
+        {part2} {harjoitus2}
+      </p>
+      <p>
+        {part3} {harjoitus3}
+      </p>
     </div>
   )
 }
 
+const Total = ({harjoitus1, harjoitus2, harjoitus3}) => {
+  const totalExercises = harjoitus1 + harjoitus2 + harjoitus3;
+  return (
+    <p>Number of exercises {totalExercises}</p>
+  )
+}
 
+const App = () => {
+  const kurssi = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const harjoitus1 = 10;
+  const part2 = 'Using props to pass data';
+  const harjoitus2 = 7;
+  const part3 = 'State of a component';
+  const harjoitus3 = 14;
 
-export default App
+  return (
+    <div>
+      <Header kurssi={kurssi} />
+      <Content
+        part1={part1}
+        harjoitus1={harjoitus1}
+        part2={part2}
+        harjoitus2={harjoitus2}
+        part3={part3}
+        harjoitus3={harjoitus3}
+      />
+      <Total
+        harjoitus1={harjoitus1}
+        harjoitus2={harjoitus2}
+        harjoitus3={harjoitus3}
+      />
+    </div>
+  );
+};
+
+export default App;

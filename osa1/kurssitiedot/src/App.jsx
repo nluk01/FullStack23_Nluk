449beabcd6e72dbf33aaa6121e731f8d75a46c0e
Nun <nun.lukkarinen01@gmail.com>
2023-12-26 13:39:29 +0200

    Päivitetty 1.2

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,53 +1,48 @@
-const Header = ({ course }) => {
+const Header = (props) => {
   return (
-    <h1>{course}</h1>
+    <h1>{props.kurssi}</h1>
   )
 }
 
-const Part = ({part, exercises}) => {
+const Part = (props) => {
   return (
-    <p>
-      {part} {exercises}
-    </p>
+    <p>{props.part} {props.lkm}</p>
   )
 }
 
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part, index) => (
-        <Part key={index} part={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  )
+const Content = (props) => {
+  <div>
+      <Part part={props.part1} lkm={props.harjoitus1} />
+      <Part part={props.part2} lkm={props.harjoitus2} />
+      <Part part={props.part3} lkm={props.harjoitus3} />
+  </div>
 }
 
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  const totalExercises = exercises1 + exercises2 + exercises3;
+const Total = (props) => {
   return (
-    <p>Number of exercises {totalExercises}</p>
-  );
-};
+    <p>Number of exercise {props.yhteensa}</p>
+  )
+}
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ]
+  const kurssi = 'Half Stack application development';
 
-  const [part1, part2, part3] = parts; 
+  const part1 = 'Fundamentals of React';
+  const harjoitus1 = 10;
+
+  const part2 = 'Using props to pass data';
+  const harjoitus2 = 7;
+
+  const part3 = 'State of component';
+  const harjoitus3 = 14;
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Header kurssi={kurssi} />
+      <Part part={part1} lkm={harjoitus1} />
+      <Part part={part2} lkm={harjoitus2} />
+      <Part part={part3} lkm={harjoitus3} />
+      <Total yhteensa={harjoitus1 + harjoitus2 + harjoitus3} />
     </div>
   )
 }
